ed55e746551785654ba98f146b7118fe3c8ded56
Robin van den Berg <robinofski.personal@gmail.com>
2026-04-27 15:28:15 +0200

    chore: update wezterm keybinds

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -40,13 +40,31 @@ config.front_end = "WebGpu"
 -- Scrollback
 config.scrollback_lines = 10000
 
--- macOS/Linux copy-paste friendly bindings
+-- macOS/Linux copy-paste friendly bindings + panel and tab management with ALT/Option key
 config.keys = {
   { key = "c", mods = "CMD", action = wezterm.action.CopyTo("Clipboard") },
   { key = "v", mods = "CMD", action = wezterm.action.PasteFrom("Clipboard") },
   { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
   { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
   { key = "Enter", mods = "ALT", action = wezterm.action.ToggleFullScreen },
+  { key = "t", mods = "ALT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+  { key = "w", mods = "ALT", action = wezterm.action.CloseCurrentTab { confirm = true } },
+  -- Split pane with ALT + Shift + Enter and close with ALT + Shift + W
+  { key = "Enter", mods = "ALT|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "Enter", mods = "ALT|SHIFT|CTRL", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "w", mods = "ALT|SHIFT", action = wezterm.action.CloseCurrentPane { confirm = true } },
+  -- Pane management with ALT + Arrow keys, and resizing with ALT + SHIFT + Arrow keys
+  { key = "LeftArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+  { key = "RightArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+  { key = "UpArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "DownArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+  { key = "LeftArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Left", 5 } },
+  { key = "RightArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Right", 5 } },
+  { key = "UpArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Up", 5 } },
+  { key = "DownArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Down", 5 } },
+  -- Tab management with ALT + Tab and ALT + SHIFT + Tab
+  { key = "Tab", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+  { key = "Tab", mods = "ALT|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
 }
 
 return config
